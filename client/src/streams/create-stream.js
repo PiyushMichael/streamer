78b8cd22f8bf8fbe4f4def691856fa39cfdec5a1
PiyushMichael <piyush.michael93@gmail.com>
2019-06-28 23:08:18 +0530

    redux forms added

--- client/src/streams/create-stream.js
+++ client/src/streams/create-stream.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {Field, reduxForm} from 'redux-form';
 import {Link} from 'react-router-dom';
 
 class CreateStream extends React.Component{
 	render(){
-		return <div>page to create new stream</div>
+		return (
+		<div>
+			page to create new stream
+			<form>
+				<Field name="title" />
+				<Field name="description" />
+			</form>
+		</div>);
 	}
 }
 
-export default CreateStream;
+export default reduxForm({form: 'streamCreate'})(CreateStream);
