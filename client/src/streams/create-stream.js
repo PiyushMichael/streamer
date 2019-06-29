f4917173ec5704b0befe4a84aa5d05614b1063c7
PiyushMichael <PiyushMichael@users.noreply.github.com>
2019-06-29 17:23:38 +0530

    form  complete

--- client/src/streams/create-stream.js
+++ client/src/streams/create-stream.js
@@ -21,7 +21,7 @@ class CreateStream extends React.Component{
 		return (
 		<div>
 			page to create new stream
-			<form className="ui form" onSubmit={this.props.handleSubmit(this.onSubmit)}>
+			<form className="ui form error" onSubmit={this.props.handleSubmit(this.onSubmit)}>
 				<Field name="title" component={this.renderInput} label="Enter Title: "/>
 				<Field name="description" component={this.renderInput} label="Enter Description: "/>
 				<button className="ui button primary">Submit</button>
